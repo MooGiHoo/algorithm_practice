ef18bff4c8fc41a82540867aa5215af019908ffc
kimnamgyu <lethargic.hood@gmail.com>
2023-01-26 17:49:46 +0900

    feat : partial_increasing_sequence(피보나치 아닌 버전)

---
+++ src/src/DP/partial_increasing_sequence_2.java
@@ -0,0 +1,43 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class partial_increasing_sequence_2 {
+    static int[] dy;
+    public static int solution(int[] arr){
+        int answer = 0;
+        dy[0] = 1;
+        dy = new int[arr.length];
+        for(int i=1; i<arr.length; i++) {
+            int max=0;
+            for(int j=i-1; j>=0; j--) {
+                if(arr[j]<arr[i] && dy[j] > max) max = dy[j];
+            }
+            dy[i] = max+1;
+            answer = Math.max(dy[i], answer);
+        }
+        return answer;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        dy = new int[n];
+        System.out.println(solution(arr));
+    }
+}
+
+
+
+
+
+
+
+
+
+
